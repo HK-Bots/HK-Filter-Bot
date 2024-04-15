c78a3bea594123a71d14ad3239896b9f479509d3
HK_Bots <138287253+HK-Bots@users.noreply.github.com>
2024-04-16 01:27:16 +0530

    Update custom_dl.py

--- TechHK/util/custom_dl.py
+++ TechHK/util/custom_dl.py
@@ -3,12 +3,12 @@ import asyncio
 import logging
 from info import *
 from typing import Dict, Union
-from TechVJ.bot import work_loads
+from TechHK.bot import work_loads
 from pyrogram import Client, utils, raw
-from TechVJ.util.file_properties import get_file_ids
+from TechHK.util.file_properties import get_file_ids
 from pyrogram.session import Session, Auth
 from pyrogram.errors import AuthBytesInvalid
-from TechVJ.server.exceptions import FIleNotFound
+from TechHK.server.exceptions import FIleNotFound
 from pyrogram.file_id import FileId, FileType, ThumbnailSource
 
 
