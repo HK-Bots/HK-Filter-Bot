270945075ab1f4f1fe50207e0d5e41721c462ebf
HK_Bots <138287253+HK-Bots@users.noreply.github.com>
2024-05-03 20:35:10 +0530

    Update share_text.py

--- plugins/Extra/share_text.py
+++ plugins/Extra/share_text.py
@@ -6,24 +6,18 @@ from pyrogram import Client, filters
 from urllib.parse import quote
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-@Client.on_message(filters.command(["share_text", "share", "sharetext",]))
+@Client.on_message(filters.command(["share_text", "share", "sharetext"]))
 async def share_text(client, message):
-    reply = message.reply_to_message
-    reply_id = message.reply_to_message.id if message.reply_to_message else message.id
-    input_split = message.text.split(None, 1)
-    if len(input_split) == 2:
-        input_text = input_split[1]
-    elif reply and (reply.text or reply.caption):
-        input_text = reply.text or reply.caption
+    hk = await client.ask(chat_id = message.from_user.id, text = "Now Send me your text.")
+    if hk and (hk.text or hk.caption):
+        input_text = hk.text or hk.caption
     else:
-        await message.reply_text(
-            text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",                
-            reply_to_message_id=reply_id,               
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates channel", url=f"https://t.me/h_k_Bots")]])
+        await hk.reply_text(
+            text=f"**Notice:**\n\n1. Send Any Text Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",               
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates Channel", url=f"https://t.me/h_k_Bots")]])
             )                                                   
         return
-    await message.reply_text(
+    await hk.reply_text(
         text=f"**Here is Your Sharing Text 👇**\n\nhttps://t.me/share/url?url=" + quote(input_text),
-        reply_to_message_id=reply_id,
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("♂️ Share", url=f"https://t.me/share/url?url={quote(input_text)}")]])       
     )
